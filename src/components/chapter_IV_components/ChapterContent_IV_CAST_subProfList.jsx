4431af839d97edaa255f0d2ae69fe6eeeaa7700f
JaroslawJaniak <JaroslawJaniak>
2024-07-18 21:52:39 +0200

    chapter 4 style change

--- src/components/chapter_IV_components/ChapterContent_IV_CAST_subProfList.jsx
+++ src/components/chapter_IV_components/ChapterContent_IV_CAST_subProfList.jsx
@@ -14,37 +14,39 @@ export const ChapterContent_IV_CAST_subProfList = (props) => {
   
   return (
     <div
-      className="ml-4 border [&>*]:lowercase"
+      className="ml-4 border [&>*]:lowercase grid grid-cols-2"
       hidden={!(context.firstProfession === props.mainProf.profName)}
     >
-      {props.mainProf.combiningProfessions.map((subProfList) =>
-        subProfList
-          .filter((subProfName) =>
-            props.professionsList.some(
-              (raceProf) => raceProf.profName === subProfName
+      {props.mainProf.combiningProfessions.map((subProfList) => (
+        <div className="border rounded bg-white  m-1 p-1">
+          {subProfList
+            .filter((subProfName) =>
+              props.professionsList.some(
+                (raceProf) => raceProf.profName === subProfName
+              )
             )
-          )
-          .map((subProf) => (
-            <p>
-              <InputComponent
-                type={"checkbox"}
-                name={subProf}
-                className={"lowercase"}
-                checked={
-                  context.secondProfession === subProf &&
-                  context.firstProfession === props.mainProf.profName &&
-                  context.secondProfessionChecked
-                }
-                onChange={() => {
-                  handleElementChange(subProf);
-                }}
-                disabled={
-                  !(context.firstProfession === props.mainProf.profName)
-                }
-              />
-            </p>
-          ))
-      )}
+            .map((subProf) => (
+              <p>
+                <InputComponent
+                  type={"checkbox"}
+                  name={subProf}
+                  className={"lowercase"}
+                  checked={
+                    context.secondProfession === subProf &&
+                    context.firstProfession === props.mainProf.profName &&
+                    context.secondProfessionChecked
+                  }
+                  onChange={() => {
+                    handleElementChange(subProf);
+                  }}
+                  disabled={
+                    !(context.firstProfession === props.mainProf.profName)
+                  }
+                />
+              </p>
+            ))}{" "}
+        </div>
+      ))}
     </div>
   );
 };
