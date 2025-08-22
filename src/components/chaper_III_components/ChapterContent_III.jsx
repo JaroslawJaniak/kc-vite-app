6a5de126ab962edf36e868a5abc13600c3585bc4
JaroslawJaniak <j.janiak81@gmail.com>
2025-08-22 20:03:51 +0200

    chapterIII component style chenged

--- src/components/chaper_III_components/ChapterContent_III.jsx
+++ src/components/chaper_III_components/ChapterContent_III.jsx
@@ -1,4 +1,4 @@
-import chp3_info from "./chp3_info";
+
 import { ChapterContent_III_disabilities } from "./ChapterContent_III_disabilities";
 import { ChapterContent_III_abilities } from "./ChapterContent_III_abilities";
 
@@ -6,15 +6,9 @@ const ChapterContent_III = () => {
   return (
     <section className="section mb-20">
       <h2>III. LOSOWANIE UŁOMNOŚCI I ZDLONOŚCI NADNATURALNYCH</h2>
-      {/* <ChapterContent_III_disabilities />
-      <ChapterContent_III_abilities /> */}
+
       <ChapterContent_III_disabilities />
       <ChapterContent_III_abilities />
-      <div className="border rounded md:w-96 h-32 m-auto mt-4 flex justify-center shadow-md">
-        <p className="text-xl m-auto font-mono font-bold text-red-900">
-          ROZDZIAŁ W ROZBUDOWIE...
-        </p>
-      </div>
     </section>
   );
 };
